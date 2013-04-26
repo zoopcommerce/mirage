a5dcf826244b92b93ff9dd361b4dd9715506f210
Josh Stuart <dev@zoopcommerce.com>
2013-04-26 22:16:29 +1000

    Moved files to correct namespace directories

---
+++ src/Zoop/Google/ImageCharts/Autoloader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Zoop\Google\ImageCharts;
+
+/**
+ * Autoloads ImageCharts classes.
+ *
+ * @author Josh Stuart <josh.stuart@zoopcommerce.com>
+ */
+class Autoloader {
+
+    /**
+     * Registers an SPL autoloader.
+     */
+    public static function register() {
+        ini_set('unserialize_callback_func', 'spl_autoload_call');
+        spl_autoload_register(array(new self, 'autoload'));
+    }
+
+    /**
+     * Handles autoloading of classes.
+     *
+     * @param string $class A class name.
+     */
+    public static function autoload($class) {
+        if (0 !== strpos($class, 'ImageCharts')) {
+            return;
+        }
+        die(var_dump($class));
+        if (is_file($file = dirname(__FILE__) . '/../' . str_replace(array('_', "\0"), array('/', ''), $class) . '.php')) {
+            require_once $file;
+        }
+    }
+
+}
